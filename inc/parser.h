65c2dc6d433034d8e1e6bdf1c411d7c045e83dec
MaciejGd <maciekzgk@gmail.com>
2024-06-02 19:12:15 +0200

    Code cleaned

--- inc/parser.h
+++ inc/parser.h
@@ -4,11 +4,4 @@
 #include "lex.h"
 #include "production.h"
 
-//two functions for loading tokens from file
-TYPE stringToType(string key);
-
-int getElement(vector<vector<int>>& prod);
-
 void parse(vector<Token>& tokens);
-void error(string message, int line_num);
-
--- src/parser.cpp
+++ src/parser.cpp
@@ -2,45 +2,6 @@
 
 using std::string, std::vector, std::cout, std::cin, std::endl, std::find;
 
-TYPE stringToType(string key)
-{
-	TYPE ret_val;
-	if (key == "Op")
-		ret_val = TYPE::Op;
-	else if (key=="Sep")
-		ret_val = TYPE::Sep;
-	else if (key=="Id")
-		ret_val = TYPE::Id;
-	else if (key=="Int")
-		ret_val = TYPE::Int;
-	else if (key=="Float")
-		ret_val = TYPE::Float;
-	else if (key=="Str")
-		ret_val = TYPE::Str;
-	else if (key=="Float")
-		ret_val = TYPE::Float;
-	else if (key=="Bool")
-		ret_val = TYPE::Bool;
-	else if (key=="Key")
-		ret_val = TYPE::Key;
-	else if (key=="Lit")
-		ret_val = TYPE::Lit;
-	return ret_val;
-}
-
-int getElement(vector<vector<int>>& prod, vector<string> token_types, Token* curr_token)
-{
-	string token_val;
-	if (curr_token->type == TYPE::Id)
-		token_val = "id";
-	else if (curr_token->type == TYPE::Str || curr_token->type == TYPE::Int || curr_token->type == TYPE::Float)
-		token_val = "const";
-	else 
-		token_val = curr_token->value;
-	auto index = std::find(token_types.begin(), token_types.end(), token_val);
-	return 0;	
-}
-
 void parse(vector<Token>& tokens)
 {
 	int counter = 0;
@@ -62,7 +23,7 @@ void parse(vector<Token>& tokens)
 		}
 		else 
 		{
-			//get from parsing table wanted expression
+			//count x and y coordinates from parsing table
 			int terminal_index;
 			if (tokens[counter].type == TYPE::Id)
 				terminal_index = 5;
@@ -70,8 +31,8 @@ void parse(vector<Token>& tokens)
 				terminal_index = 6;
 			else 
 				terminal_index = std::find(terminals.begin(), terminals.end(), tokens[counter].value) - terminals.begin();
+			//from the parsing table get a grammar rule to parse
 			int statement_index = parsing_table[parse_symbol][terminal_index];
-			
 			if (statement_index == 0)
 			{
 				cout << " Error while parsing non-terminal: " << symbols_map[parse_symbol] << " in token: " << tokens[counter].value << "\n";
